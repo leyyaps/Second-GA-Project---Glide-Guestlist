7f94b73b8ba6b3a714f0e09b7536d1d8d93cc068
leyyaps <andrew.smith@ipsofactouk.com>
2016-08-05 17:59:08 +0100

    removing events table before scaffolding

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-
+  has_many :events
   mount_uploader :profile_pic, ProfilePicUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
