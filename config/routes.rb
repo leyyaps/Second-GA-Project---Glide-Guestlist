c6572ed2d52bc43a08ac2eef5c01020332fd6985
leyyaps <andrew.smith@ipsofactouk.com>
2016-08-05 18:06:05 +0100

    scaffolded events. beer time...

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :events
   devise_for :users
   root "statics#homepage"
   get 'users/:id', to: 'users#show', as: "user"
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160805165725) do
+ActiveRecord::Schema.define(version: 20160805170039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string   "name"
+    t.date     "date"
+    t.text     "description"
+    t.integer  "allocation"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_events_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
@@ -41,4 +52,5 @@ ActiveRecord::Schema.define(version: 20160805165725) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "events", "users"
 end
